6d2d0e7bfc0245cb5da5411215c61f6478db110d
Alex <paracosm17@yandex.ru>
2023-12-11 18:06:47 +1000

    shop admin

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,63 @@
 from django.contrib import admin
+from .models import (
+    Supplier,
+    ProductOrder,
+    SupplierInvoice,
+    Order,
+    Product,
+    CartProduct,
+    Category,
+    ProductCategory,
+    Warehouse,
+    Manager
+)
 
-# Register your models here.
+
+@admin.register(Supplier)
+class SupplierAdmin(admin.ModelAdmin):
+    list_display = ('supplier_code', 'address', 'phone')
+
+
+@admin.register(ProductOrder)
+class ProductOrderAdmin(admin.ModelAdmin):
+    list_display = ('product_code', 'supplier_order_number', 'quantity')
+
+
+@admin.register(SupplierInvoice)
+class SupplierInvoiceAdmin(admin.ModelAdmin):
+    list_display = ('supplier_order_number', 'manager_code', 'amount', 'payment_date')
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('supplier_order_number', 'manager_code', 'supplier_code', 'order_date', 'status')
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('product_code', 'name', 'availability_status', 'price')
+
+
+@admin.register(CartProduct)
+class CartProductAdmin(admin.ModelAdmin):
+    list_display = ('product', 'order_number', 'quantity')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('category_code', 'name')
+
+
+@admin.register(ProductCategory)
+class ProductCategoryAdmin(admin.ModelAdmin):
+    list_display = ('product', 'category')
+
+
+@admin.register(Warehouse)
+class WarehouseAdmin(admin.ModelAdmin):
+    list_display = ('product', 'available_quantity', 'reserved_quantity')
+
+
+@admin.register(Manager)
+class ManagerAdmin(admin.ModelAdmin):
+    list_display = ('manager_code', 'first_name', 'last_name', 'middle_name', 'phone')
